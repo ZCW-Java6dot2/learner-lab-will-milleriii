4143306c896890a77b6521c8382eb86c39da60cc
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-21 12:32:48 -0500

    Person and person test created

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,26 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private String name;
+    private final long id;
+
+
+
+    public Person(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,33 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor(){
+        String expectedName = "Will";
+        long expectedId = 1;
+
+        Person will = new Person(expectedId, expectedName);
+
+        Assert.assertEquals(expectedId, will.getId());
+        Assert.assertEquals(expectedName, will.getName());
+    }
+
+    @Test
+    public void testSetName(){
+        String expectedName = "Will";
+        long expectedId = 1;
+
+        Person will = new Person(expectedId, expectedName);
+        String actualName = "Miller";
+
+        will.setName(actualName);
+
+        Assert.assertEquals(actualName, will.getName());
+    }
+
 
 }

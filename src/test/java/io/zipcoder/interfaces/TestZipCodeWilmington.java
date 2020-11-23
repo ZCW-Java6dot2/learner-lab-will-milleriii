82f4f8a4729687500393e19ae2270f132cd0df25
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-22 21:02:55 -0500

    working on ZCW class

---
+++ src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
@@ -0,0 +1,20 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TestZipCodeWilmington {
+
+    @Before
+    public void setup(){
+        /*ZipCodeWilmington.fireStaff();*/
+    }
+
+    @Test
+    public void testFireStaff(){
+        ArrayList<Instructor> testList = new ArrayList<Instructor>();
+
+    }
+}
